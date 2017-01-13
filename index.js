01d631612262477a518eda642b11c60e5001d159
Dara Hayes <dara.hayes@nearform.com>
2017-01-13 16:52:35 +0000

    working implementation with pipes

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 #!/usr/bin/env node
 'use strict'
 const program = require('commander')
-const ecsDeploy = require('./lib/deploy')
+const run = require('./lib/run')
 
 program
   .version('0.0.1')
-  .command('deploy [task]')
   .description('Deploy a task definition to a given ECS cluster')
+  .option('-t --task [taskDefinition]', 'The task definition to create')
   .option('-s, --service [service]', 'The service to update')
   .option('-c --cluster [cluster]', 'The cluster the service is defined in')
-  .action(runDeploy)
+  .parse(process.argv)
 
-program.parse(process.argv)
-
-function runDeploy (task, options) {
-  console.log('task:', task)
-  console.log('service', options.service)
-  console.log('cluster', options.cluster)
-
-  if (!options.service) {
-    console.log('Service argument is required')
-    process.exit(1)
-  }
-
-  if (!options.cluster) {
-    console.log('Cluster argument is required')
-    process.exit(1)
-  }
-
-  ecsDeploy(task, options.service, options.cluster)
+if (!process.stdin.isTTY) {
+  var pipe = ''
+  process.stdin.setEncoding('utf8')
+  process.stdin.on('data', (chunk) => { pipe += chunk })
+  process.stdin.on('end', () => {
+    program.taskDefinition = pipe.trim()
+    run(program)
+  })
+} else {
+  run(program)
 }
