1580db9ee52ea62a12bdd475269c740e101dd613
Dara Hayes <dara.hayes@nearform.com>
2017-01-16 15:12:52 +0000

    tests for run.js

---
+++ test/run.test.js
@@ -0,0 +1,33 @@
+const proxyquire = require('proxyquire')
+const tape = require('tape')
+
+const run = proxyquire('../lib/run.js', {
+  './deploy': (opts, callback) => {
+    callback(null, {deployed: true})
+  }
+})
+
+tape('run.js calls the deploy function', (test) => {
+  test.plan(2)
+  let opts = {
+    taskDefinition: 'myTask',
+    service: 'myService',
+    cluster: 'myCluster'
+  }
+  run(opts, (err, result) => {
+    test.error(err, 'no error is present')
+    test.equal(result.deployed, true, 'deploy function has been called')
+  })
+})
+
+tape('run.js will not call the deploy function if arguments are wrong', (test) => {
+  test.plan(2)
+  let opts = {
+    taskDefinition: 'myTask',
+    cluster: 'myCluster'
+  }
+  run(opts, (err, result) => {
+    test.ok(err, 'an error occurred')
+    test.equal(err.message, 'invalid args', 'invalid args error')
+  })
+})
