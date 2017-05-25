6927deb4d916068d856ac9c8bb22dea8441bf9e7
Dara Hayes <dara.hayes@nearform.com>
2017-05-25 14:05:24 +0100

    add tests for taskFile parameter

--- test/run.test.js
+++ test/run.test.js
@@ -1,5 +1,6 @@
 const proxyquire = require('proxyquire')
 const tape = require('tape')
+const path = require('path')
 
 const run = proxyquire('../lib/run.js', {
   './deploy': (opts, callback) => {
@@ -11,6 +12,19 @@ tape('run.js calls the deploy function', (test) => {
   test.plan(2)
   var opts = {
     taskDefinition: JSON.stringify({foo: 'bar'}),
+    service: 'myService',
+    cluster: 'myCluster'
+  }
+  run(opts, (err, result) => {
+    test.error(err, 'no error is present')
+    test.equal(result.deployed, true, 'deploy function has been called')
+  })
+})
+
+tape('run.js works with a filename', (test) => {
+  test.plan(2)
+  var opts = {
+    taskFile: path.resolve(__dirname, './data/taskDef.json'),
     service: 'myService',
     cluster: 'myCluster'
   }
@@ -36,6 +50,18 @@ tape('run.js throws if invalid taskDefinition JSON is supplied', (test) => {
   test.plan(1)
   var opts = {
     taskDefinition: 'some: bad: JSON',
+    service: 'myService',
+    cluster: 'myCluster'
+  }
+  test.throws(() => {
+    run(opts)
+  })
+})
+
+tape('run.js throws if taskFile does not exist', (test) => {
+  test.plan(1)
+  var opts = {
+    taskFile: '/some/file',
     service: 'myService',
     cluster: 'myCluster'
   }
